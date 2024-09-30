e62ba30f97b95a4a13d273d38bf2b177cf61e753
kiruthika-cse-cs <kiruthikar328@gmail.com>
2024-09-30 12:08:14 -0400

    Create simpleintrest.c

---
+++ simpleintrest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float farenheit,celsius;
+    printf("enter farenheit:");
+    scanf("%f",&farenheit);
+    celsius=(farenheit-32)*1.8;
+    printf("%f",celsius);
+    
+}
