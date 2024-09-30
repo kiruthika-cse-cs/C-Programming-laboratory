788543cb53d87da7a37e1388a609c6fb4a589fe7
kiruthika-cse-cs <kiruthikar328@gmail.com>
2024-09-30 15:27:10 +0530

    Create size.c

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int inttype;
+    float floattype;
+    double doubletype;
+    char chartype;
+    printf("size of int :%d bytes\n",sizeof(inttype));
+    printf("size of float:%d bytes\n",sizeof(floattype));
+    printf("size of char:%d bytes\n",sizeof(chartype));
+    printf("size of double:%d bytes\n",sizeof(doubletype));
+    
+}
