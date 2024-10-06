aff2add8141ced193e669e0d112ece25d020f145
kiruthika-cse-cs <kiruthikar328@gmail.com>
2024-10-06 14:57:59 +0530

    Create largest num.c

---
+++ largest num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+   int a,b;
+   scanf("%d%d",&a,&b);
+   if(a>b)
+   {
+       printf("\"A\"is largest");
+   } 
+   else
+   {
+       printf("\"B\"is greater");
+       
+   }
+}
