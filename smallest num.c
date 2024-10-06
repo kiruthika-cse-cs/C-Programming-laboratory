c50c67587b344515d97117bbda2e02ffb867337a
kiruthika-cse-cs <kiruthikar328@gmail.com>
2024-10-06 15:00:52 +0530

    Create smallest num.c

---
+++ smallest num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+   int a,b;
+   scanf("%d%d",&a,&b);
+   if(a<b)
+   {
+       printf("\"A\"is smallest");
+   } 
+   else
+   {
+       printf("\"B\"is smallest");
+       
+   }
+}
